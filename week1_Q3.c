81a954c6cb5ecbb424a3a77c5de2e3d108719a91
rohitpatnaik007 <99547690+rohitpatnaik007@users.noreply.github.com>
2022-02-13 15:10:50 +0530

    solutions of week 1

---
+++ week1_Q3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the number to be checked");
+    scanf("%d" , &a);
+    b=a%2;
+    switch(b)
+    {
+    case 0:
+        printf("the number is even  %d" , a);
+        break;
+    case 1:
+        printf("the number is odd   %d" , a);
+        break;
+    }   
+
+    return 0;        
+}
---
+++ week1_Q4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,x;
+    float m;
+    printf("If you want addition choose ...1...  \n  If you want subtraction choose ...2...  \n  If you want multiplication choose ...3...  \n  If you want division choose ...4...  \n  ::");
+    scanf("%d",&c);
+    printf("Enter two numbers you want to do the operation \n ::");
+    scanf("%d %d" ,&a ,&b);
+    switch(c)
+    {
+        case 1:
+            x=a+b;
+            printf("the sum of the two numbers is  : %d " ,x);
+            break;
+        case 2:
+            x=a-b;
+            printf("the difference of the two numbers is  : %d  " ,x);
+            break;
+        case 3:
+            x=a*b;
+            printf("the product of the numbers is  : %d   " ,x);
+            break;
+        case 4:
+            m=a/(float)b;
+            printf("the quotient of the numbeers is  : %f  " ,m);
+            break;
+        default:
+            printf("please choose from the available options ");
+
+    }
+    return 0;
+}
